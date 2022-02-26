06b67891841a9286e3da075906f8fa197ca32730
ArihitoMatsuda <arihito.m@gmail.com>
2022-02-26 10:32:02 +0900

    01_Rooting

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { RootRouter } from './Route'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RootRouter />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
