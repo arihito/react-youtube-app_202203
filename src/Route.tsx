62dfcb6452a78d9e97727087c9c88e824b444fe5
ArihitoMatsuda <arihito.m@gmail.com>
2022-02-26 12:27:13 +0900

    05_Main

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,20 +1,21 @@
 import { Navigate, useRoutes } from 'react-router-dom'
-
 import { HomeLayout } from './layouts/Home'
 import { SideLessHomeLayout } from './layouts/SideLessHome'
 import { SimpleLayout } from './layouts/Simple'
+import { Home } from './pages/Home'
 
 export const RootRouter = () => {
   return useRoutes([
     {
       element: <HomeLayout />,
-      children: [{ path: '/', element: <div>Home</div> }],
+      children: [{ path: '/', element: <Home /> }],
     },
     {
       element: <SideLessHomeLayout />,
+
       children: [
         { path: 'watch', element: <Navigate to='/' /> },
-        { path: 'watch/:videoId', element: <div>watch</div> }
+        { path: 'watch/:videoId', element: <div>watch</div> },
       ],
     },
     {
@@ -24,7 +25,7 @@ export const RootRouter = () => {
         { path: 'signup', element: <div>新規作成</div> },
         { path: 'forget', element: <div>パスワードリセット</div> },
         { path: '404', element: <div>Not Found</div> },
-        { path: '*', element: <Navigate to='/404' /> },
+        { path: '*', element: <Navigate to="/404" /> },
       ],
     },
     { path: '*', element: <Navigate to="/404" /> },
