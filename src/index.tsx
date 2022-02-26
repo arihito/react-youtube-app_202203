daf67a3b7e5eb3467b15e24dbaa0ea4452f2a49e
ArihitoMatsuda <arihito.m@gmail.com>
2022-02-26 10:44:09 +0900

    02_headerBar

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { RootRouter } from './Route'
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
+
+const theme = createTheme()
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RootRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,1 +1,18 @@
-export {}
+import { AppBar, Toolbar, IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { Logo } from '../../components/Logo'
+
+export const DashboardHeader = () => {
+  return (
+    <AppBar elevation={0} color="inherit">
+      <Toolbar>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <div>
+          <Logo />
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
+}
